4a6ec6208122ce31458a07df09c790ceabe201b2
kangbohyeon <qhgus535@naver.com>
2024-01-22 20:37:43 +0900

    20240121~0122 study

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,8 +1,25 @@
 import Counter from "./components/Counter";
 import Info from "./components/Info";
-function App() {
-    // return <Counter />;
-    return <Info />;
-}
+import React, { useState } from "react";
+// function App() {
+//     // return <Counter />;
+//     return <Info />;
+// }
+const App = () => {
+    const [visible, setVisble] = useState(false);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setVisble(!visible);
+                }}
+            >
+                {visible ? "숨기기" : "보이기"}
+            </button>
+            <hr />
+            {visible && <Info />}
+        </div>
+    );
+};
 
 export default App;
