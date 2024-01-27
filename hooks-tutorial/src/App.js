d6c731d579916aaad8f8af36ac62d7ed5dc1f14b
kangbohyeon <qhgus535@naver.com>
2024-01-27 15:36:20 +0900

    202400127 study 1. hook 1.1 usestate 1.2 useEffect 1.3 useReducer 1.4 useMemo 1.5 useCallback 1.6 useRef

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,25 +1,27 @@
 import Counter from "./components/Counter";
 import Info from "./components/Info";
 import React, { useState } from "react";
-// function App() {
-//     // return <Counter />;
-//     return <Info />;
-// }
-const App = () => {
-    const [visible, setVisble] = useState(false);
-    return (
-        <div>
-            <button
-                onClick={() => {
-                    setVisble(!visible);
-                }}
-            >
-                {visible ? "숨기기" : "보이기"}
-            </button>
-            <hr />
-            {visible && <Info />}
-        </div>
-    );
-};
+import Average from "./components/Average";
+function App() {
+    // return <Counter />;
+    // return <Info />;
+    return <Average />;
+}
+// const App = () => {
+//     const [visible, setVisble] = useState(false);
+//     return (
+//         <div>
+//             <button
+//                 onClick={() => {
+//                     setVisble(!visible);
+//                 }}
+//             >
+//                 {visible ? "숨기기" : "보이기"}
+//             </button>
+//             <hr />
+//             {visible && <Info />}
+//         </div>
+//     );
+// };
 
 export default App;
--- hooks-tutorial/src/components/Info.js
+++ hooks-tutorial/src/components/Info.js
@@ -1,56 +1,116 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+import useInput from "./useInput";
 
+// const Info = () => {
+//     const [name, setName] = useState("");
+//     const [nickname, setNickname] = useState("");
+
+//     // useEffect(() => {
+//     //     console.log("렌더링이 완료되었습니다.");
+//     //     console.log({
+//     //         name,
+//     //         nickname,
+//     //     });
+//     // });
+//     // useEffect(() => {
+//     //     console.log("마운트될 때만 실행됩니다.");
+//     // }, [name]);
+//     // useEffect(() => {
+//     //     console.log(name);
+//     // }, [name]);
+//     // useEffect(() => {
+//     //     console.log("effect");
+//     //     console.log(name);
+//     //     return () => {
+//     //         console.log("cleanup");
+//     //         console.log(name);
+//     //     };
+//     // });
+//     useEffect(() => {
+//         console.log("effect");
+//         console.log(name);
+//         return () => {
+//             console.log("cleanup");
+//             console.log(name);
+//         };
+//     }, [name]);
+//     const onChangeName = (e) => {
+//         setName(e.target.value);
+//     };
+
+//     const onChangeNickname = (e) => {
+//         setNickname(e.target.value);
+//     };
+
+//     return (
+//         <div>
+//             <div>
+//                 <input value={name} onChange={onChangeName}></input>
+//                 <input value={nickname} onChange={onChangeNickname}></input>
+//             </div>
+//             <div>
+//                 <div>
+//                     <b>이름 : </b>
+//                     {name}
+//                 </div>
+//                 <div>
+//                     <b>닉네임 : </b>
+//                     {nickname}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+function reducer(state, action) {
+    return {
+        ...state,
+        [action.name]: action.value,
+    };
+}
+
+// const Info = () => {
+//     const [state, dispatch] = useReducer(reducer, {
+//         name: "",
+//         nickname: "",
+//     });
+//     const { name, nickname } = state;
+//     const onChange = (e) => {
+//         dispatch(e.target);
+//     };
+//     return (
+//         <div>
+//             <div>
+//                 <input name="name" value={name} onChange={onChange} />
+//                 <input name="nickname" value={nickname} onChange={onChange} />
+//             </div>
+//             <div>
+//                 <div>
+//                     <b>이름:</b> {name}
+//                 </div>
+//                 <div>
+//                     <b>닉네임 : </b>
+//                     {nickname}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 const Info = () => {
-    const [name, setName] = useState("");
-    const [nickname, setNickname] = useState("");
-
-    // useEffect(() => {
-    //     console.log("렌더링이 완료되었습니다.");
-    //     console.log({
-    //         name,
-    //         nickname,
-    //     });
-    // });
-    // useEffect(() => {
-    //     console.log("마운트될 때만 실행됩니다.");
-    // }, [name]);
-    // useEffect(() => {
-    //     console.log(name);
-    // }, [name]);
-    // useEffect(() => {
-    //     console.log("effect");
-    //     console.log(name);
-    //     return () => {
-    //         console.log("cleanup");
-    //         console.log(name);
-    //     };
-    // });
-    useEffect(() => {
-        console.log("effect");
-        console.log(name);
-        return () => {
-            console.log("cleanup");
-            console.log(name);
-        };
-    }, [name]);
-    const onChangeName = (e) => {
-        setName(e.target.value);
-    };
-
-    const onChangeNickname = (e) => {
-        setNickname(e.target.value);
-    };
-
+    const [state, onChange] = useInput({
+        name: "",
+        nickname: "",
+    });
+    const { name, nickname } = state;
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName}></input>
-                <input value={nickname} onChange={onChangeNickname}></input>
+                <input name="name" value={name} onChange={onChange} />
+                <input name="nickname" value={nickname} onChange={onChange} />
             </div>
             <div>
                 <div>
-                    <b>이름 : </b>
-                    {name}
+                    <b>이름:</b> {name}
                 </div>
                 <div>
                     <b>닉네임 : </b>
