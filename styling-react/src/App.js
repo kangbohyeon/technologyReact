c148d48881303e4312086a374250c6987c460b5e
kangbohyeon <qhgus535@naver.com>
2024-02-05 23:46:04 +0900

    20240205 study 1. todo list component make

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
 import SassComponent from "./SassComponent";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 class App extends Component {
     render() {
         return (
             <div>
-                <SassComponent />
+                {/* <SassComponent /> */}
+                {/* <CSSModule /> */}
+                <StyledComponent />
             </div>
         );
     }
